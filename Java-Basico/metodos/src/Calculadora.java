68e2241014d5024bfd857d9dab48b259418777f4
Nicolas <nicolasruan048@gmail.com>
2023-03-03 17:55:19 -0300

    Calculadora simples

---
+++ Java-Basico/metodos/src/Calculadora.java
@@ -0,0 +1,31 @@
+
+/*
+ * Criar uma calculadora simples
+ */
+
+public class Calculadora {
+    
+    public static void soma(double num1, double num2){
+        double resul = num1 + num2;
+
+        System.out.println("A soma de " + num1 + " mais " + num2 + " é " + resul);
+    }
+
+    public static void subtracao(double num1, double num2){
+        double resul = num1 + num2;
+
+        System.out.println("A subtração de " + num1 + " menos " + num2 + " é " + resul);
+    }
+
+    public static void divisao(double num1, double num2){
+        double resul = num1 / num2;
+
+        System.out.println("A divisão de " + num1 + " por " + num2 + " é " + resul);
+    }
+
+    public static void multiplicacao(double num1, double num2){
+        double resul = num1 + num2;
+
+        System.out.println("A multiplicação de " + num1 + " vezes " + num2 + " é " + resul);
+    }
+}
