e9400df360da24901922597ca0c47d60a3e0718a
Nicolas <nicolasruan048@gmail.com>
2023-03-05 23:02:26 -0300

    Atributos da classe Carro

---
+++ Java-Intermediario/POO/src/Carro.java
@@ -0,0 +1,8 @@
+// Atributos - Caracteristicas do objeto a ser criado
+
+public class Carro {
+    
+    String cor; // Atributo
+    String modelo; // Atributo
+    double capacidadeTanque; // Atributo
+}
