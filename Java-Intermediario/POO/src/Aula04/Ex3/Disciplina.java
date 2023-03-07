76835a07c12b542ba6924617c40880053eb0e543
Nicolas <nicolasruan048@gmail.com>
2023-03-06 23:23:41 -0300

    Associação Estrutural e Comportamental

---
+++ Java-Intermediario/POO/src/Aula04/Ex3/Disciplina.java
@@ -0,0 +1,10 @@
+// Associação - Classes se ajudam uma a outra - Estrutural (Atributos) / Comportamental (Métodos)
+//              Estrutural(Agregação) - Um pode existir sem o outro
+//              Estrutural(Composição) - Um deixa de existir sem o outro
+//              Comportamental(Dependência) - O método depende do outro
+
+package Aula04.Ex3;
+
+public class Disciplina {
+    Aluno aluno; // Associação Estrutural(Agregação)
+}
---
+++ Java-Intermediario/POO/src/Aula04/Ex3/Endereco.java
@@ -0,0 +1,5 @@
+package Aula04.Ex3;
+
+public class Endereco {
+    
+}
