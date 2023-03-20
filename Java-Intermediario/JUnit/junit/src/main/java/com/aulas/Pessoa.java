d7270879e7e86c4ae8726389bfa96bf558356815
Nicolas <nicolasruan048@gmail.com>
2023-03-20 17:39:46 -0300

    Algumas coisas mudadas/adicionadas na class Pessoa

--- Java-Intermediario/JUnit/junit/src/main/java/com/aulas/Pessoa.java
+++ Java-Intermediario/JUnit/junit/src/main/java/com/aulas/Pessoa.java
@@ -1,25 +1,20 @@
 package com.aulas;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class Pessoa {
     
     private String nome;
-    private LocalDate nascimento;
+    private LocalDateTime nascimento;
 
-    public Pessoa(String nome, LocalDate nascimento) {
+    public Pessoa(String nome, LocalDateTime nascimento) {
         this.nome = nome;
         this.nascimento = nascimento;
     }
 
-    public String getNome() { return nome; }
-
-    public LocalDate getNascimento() { return nascimento; }
-
     // Vai receber a idade entre o nascimento e a data atual
-    public int getIdade() { return (int) ChronoUnit.YEARS.between(this.nascimento, LocalDate.now()); }
+    public int getIdade() { return (int) ChronoUnit.YEARS.between(this.nascimento, LocalDateTime.now()); }
 
-    public boolean MaiorDeIdade() { return getIdade() >= 18; }
+    public boolean maiorDeIdade() { return getIdade() >= 18; }
 }
- 
--- Java-Intermediario/JUnit/junit/src/test/java/com/aulas/PessoaTest.java
+++ Java-Intermediario/JUnit/junit/src/test/java/com/aulas/PessoaTest.java
@@ -3,17 +3,25 @@ package com.aulas;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class PessoaTest {
     
     @Test
-    public void validarCalculoIdade() {
-        Pessoa pessoa = new Pessoa("Nicolas", LocalDate.of(2020, 1, 1));
+    public void calcularIdadeCorretamente() {
+        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
 
-        // Aqui estou mandando a resposta q estou esperando (no caso 2) comparar cm a q vai vir na class Pessoa
-        Assertions.assertEquals(3, pessoa.getIdade());
+        // Aqui estou mandando a resposta q estou esperando (no caso 23) comparar cm a q vai vir na class Pessoa
+        Assertions.assertEquals(23, jessica.getIdade());
+    }
+
+    @Test
+    public void retornaSeMaiorDeIdade() {
+        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+        Assertions.assertTrue(jessica.maiorDeIdade()); // Para testar se vai retornar true
+
+        Pessoa joao = new Pessoa("João", LocalDateTime.now());
+        Assertions.assertFalse(joao.maiorDeIdade()); // Para testar se vai retornar false
 
     }
 }
- 
