cb4ed3b3f6cc2abf348776ba92d24eedeab35d09
Nicolas <nicolasruan048@gmail.com>
2023-03-20 23:21:23 -0300

    Testando Exceptions

--- Java-Intermediario/JUnit/junit/src/test/java/com/aulas/ExceptionsTest.java
+++ Java-Intermediario/JUnit/junit/src/test/java/com/aulas/ExceptionsTest.java
@@ -1,5 +1,18 @@
 package com.aulas;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExceptionsTest {
     
+    @Test
+    public void validarExcecaoNaTransferencia() {
+        Conta contaOrigem = new Conta("123456", 0);
+        Conta contaDestino = new Conta("456548", 100);
+
+        TransferenciaContas transferenciaContas = new TransferenciaContas();
+
+        // Assertions.assertDoesNotThrow(() -> transferenciaContas.transfere(contaOrigem,
+        //     contaDestino, 20));
+    }
 }
