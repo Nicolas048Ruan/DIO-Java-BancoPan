d1b3c52f40b99aa6bd27305fbb8f4c112b073847
Nicolas <nicolasruan048@gmail.com>
2023-03-23 16:23:50 -0300

    Atribuição e Referência

---
+++ Java-Avancado/estrutura-dados/src/main/java/com/projeto/atribuicaoreferencia/Main.java
@@ -0,0 +1,23 @@
+package com.projeto.atribuicaoreferencia;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int intA = 1;
+        int intB = intA; // Cópia de valor
+
+        System.out.println("intA=" + intA + " intB=" + intB);
+        intA = 2;
+        System.out.println("intA=" + intA + " intB=" + intB);
+
+        // Usando objetos, se eu mudar um vai mudar todos que estão relacionados a ele
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB = objA; // Cópia de referência
+        
+        System.out.println("objA=" + objA + " objB=" + objB); // output -> objA=1 | objB=1
+        objA.setNum(2); // Mudei o valor de A
+        System.out.println("objA=" + objA + " objB=" + objB); // output -> objA=2 | objB=2
+
+    }
+}
