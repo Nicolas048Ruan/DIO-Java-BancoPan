3239fac29b663521e912874f00ab60099bb57235
Nicolas048Ruan <nicolasruan048@gmail.com>
2023-04-04 18:06:36 -0300

    implementando o getNo() e o get()

--- Java-Avancado/estrutura-dados/listas_duplamente_encadeadas/src/main/java/com/aulas/ListaDuplamenteEncadeada.java
+++ Java-Avancado/estrutura-dados/listas_duplamente_encadeadas/src/main/java/com/aulas/ListaDuplamenteEncadeada.java
@@ -5,10 +5,23 @@ public class ListaDuplamenteEncadeada<T> {
     private NoDuplo<T> primeiroNo;
     private NoDuplo<T> ultimoNo;
     private int tamanhoLista;
+
     public ListaDuplamenteEncadeada() {
         this.primeiroNo = null;
         this.ultimoNo = null;
         this.tamanhoLista = 0;
+    }
+
+    public T get(int index) { // Pega o elemento no indice q passarmos
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index) {
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i=0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
     }
 
     public int size() { return tamanhoLista; }
