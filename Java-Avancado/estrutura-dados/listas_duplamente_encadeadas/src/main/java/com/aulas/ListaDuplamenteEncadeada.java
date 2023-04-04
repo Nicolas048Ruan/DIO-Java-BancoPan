96eed467ba3f56a067d6625f930b576e031b1358
Nicolas048Ruan <nicolasruan048@gmail.com>
2023-04-04 17:46:37 -0300

    implementando o size na ListaDuplamenteEncadeada

---
+++ Java-Avancado/estrutura-dados/listas_duplamente_encadeadas/src/main/java/com/aulas/ListaDuplamenteEncadeada.java
@@ -0,0 +1,16 @@
+package com.aulas;
+
+public class ListaDuplamenteEncadeada<T> {
+    
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+    private int tamanhoLista;
+    public ListaDuplamenteEncadeada() {
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int size() { return tamanhoLista; }
+
+}
